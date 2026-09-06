477f15711cd2dbd388f5ff65ed97fe292d3dbba2
Ivan <ivanjosejinse5049@gmail.com>
2026-09-06 22:42:15 +0530

    Initial commit

---
+++ models/listing.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const listingSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: String,
+  image: {
+    filename: {
+        type: String,
+        default: "listingimage",
+    },
+    url: {
+        type: String,
+        default: "https://thumbs.dreamstime.com/b/default-image-icon-vector-missing-picture-page-website-design-mobile-app-no-photo-available-236105299.jpg",
+    },
+  },  
+  price: Number,
+  location: String,
+  country: String,
+});
+
+const Listing = mongoose.model("Listing", listingSchema);
+module.exports = Listing;
